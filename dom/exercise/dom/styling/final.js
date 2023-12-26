9c4ef9317ccafd574d4063436cc1a3579e5401dc
Indra <115783796+IngaIndra@users.noreply.github.com>
2023-12-26 15:26:21 +0800

    delete finals

--- dom/exercise/dom/styling/final.js
+++
@@ -1,3 +0,0 @@
-// Use setAttribute to set the value of a specified attribute on an element
-
-// Use style property to get or set inline styles of an element
--- dom/exercise/dom/styling/script.js
+++ dom/exercise/dom/styling/script.js
@@ -1,6 +1,3 @@
 // Use setAttribute to set the value of a specified attribute on an element
-const myElement = document.getElementById("myElement");
-myElement.setAttribute("data-info", "This is additional information");
 
 // Use style property to get or set inline styles of an element
-myElement.style.backgroundColor = "yellow";
