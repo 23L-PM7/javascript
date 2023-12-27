3d441decffc8fa97a647bf6a3e8e67db92c2e76d
Baatarvant <baatarvant@gmail.com>
2023-12-27 14:38:41 +0800

    form

--- dom/form/index.js
+++ dom/form/index.js
@@ -6,24 +6,11 @@ function getValues(event) {
 
   let data = {
     name: this.name.value,
-    "fav color": this["fav-color"].value,
-    gender: this.gender.value,
-    "fav foods": [],
   };
-  let favFoods = document.querySelectorAll(".fav-foods");
 
-  for (let food of favFoods) {
-    if (food.checked) {
-      data["fav foods"].push(food.value);
-    }
-  }
   const outputHMTL = `
 		<p>Name: <span>${data.name}</span></p>
-		<p>Fav color: <span>${data["fav color"]}</span></p>
-		<p>Gender: <span>${data.gender}</span></p>
-		<p>Fav food: <span>${data["fav foods"]}</span></p>
 	`;
-  console.log(data);
 
   outputContainer.innerHTML = outputHMTL;
 }
