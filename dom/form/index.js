929d245f88d3a43039d30ffb2a04038b717349b5
Indra <115783796+IngaIndra@users.noreply.github.com>
2023-12-28 15:27:19 +0800

    added description

--- dom/form/index.js
+++ dom/form/index.js
@@ -1,18 +1,25 @@
+//my-form nertei HTML form-iig barij avad 'form' gej sanaarai
 let form = document.forms["my-form"];
+// output ID-tei HTML element-iig olj avad 'outputContainer' gej sanaarai
 let outputContainer = document.getElementById("output");
 
+// form-iin orj irsen utgiig avah function
+// utga orj irehed ni 'data' gedeg Object-d form-n orj irsen utguudiig avna
 function getValues(event) {
+  // form-n default uildel ni server-luu huselt yvuulaad huudsaa refresh hiideg (bichsen utga alga bolno)
   event.preventDefault();
 
   let data = {
     name: this.name.value,
+    color: this.favColor.value,
   };
-
+  // form-n utgaa avsan ym chin goy haruuliy
   const outputHMTL = `
 		<p>Name: <span>${data.name}</span></p>
+    <p>Fav color: <span>${data.color}</p>
 	`;
-
+  // say deer goy haruulsan ur dungee 'outputContainer' dotor hiichvel huudas deer haragdana.
   outputContainer.innerHTML = outputHMTL;
 }
-
+// gadnaas utga orj irehed 'getValues' function-iig ajilluul
 form.onsubmit = getValues;
